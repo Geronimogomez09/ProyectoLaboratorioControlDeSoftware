498083cc22cc3ec4e88d605a9b99372f49f17523
Geronimo Gomez <geronimogomezepet20@gmail.com>
2026-06-19 11:15:04 -0300

    Cambiando color del diseño de catalogo y arreglando problema de cerrar el menu lateral

--- JS/catalogo.js
+++ JS/catalogo.js
@@ -1,6 +1,11 @@
 const botonMenu = document.getElementById("menu");
+const cerrarMenu = document.getElementById("close-menu")
 const menuLateral = document.getElementById("menu-lateral");
 
 botonMenu.addEventListener("click", () => {
     menuLateral.classList.toggle("activo");
+});
+
+cerrarMenu.addEventListener("click", () => {
+    menuLateral.classList.remove("activo");
 });
